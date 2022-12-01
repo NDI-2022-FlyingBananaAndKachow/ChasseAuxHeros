16098c137fb7f97a823d365773fc2ea482ad35d1
megat69 <assassincraft218@gmail.com>
2022-12-01 21:19:24 +0100

    Made sure levels IDs and question IDs are valid

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 from markupsafe import escape
 
+# Contient l'instance du serveur
 app = Flask(__name__, template_folder="templates/", static_folder='static/')
+
+
+# Sert à tester si le numéro de map est valide
+is_map_valid = lambda map_id: 0 < map_id < 3
+is_question_valid = lambda question_id: 0 < question_id < 10
 
 
 @app.route('/')
@@ -19,7 +25,10 @@ def level(level_id: int):
 	Contient et renvoie les maps et niveaux.
 	:param level_id: Prend le l'ID du niveau (de 1 à 3).
 	"""
-	return render_template("level.html", level_id=level_id)
+	if is_map_valid(level_id):
+		return render_template("level.html", level_id=level_id)
+	else:
+		abort(403)
 
 
 @app.route('/questions/<int:level_id>/<int:question_id>')
@@ -29,7 +38,10 @@ def question(level_id: int, question_id: int):
 	:param level_id: Prend l'ID du niveau (de 1 à 3)
 	:param question_id: Prend l'ID de la question (0 à 9).
 	"""
-	return render_template("question.html", level_id=level_id, question_id=question_id)
+	if is_map_valid(level_id) and is_question_valid(question_id):
+		return render_template("question.html", level_id=level_id, question_id=question_id)
+	else:
+		abort(403)
 
 
 @app.route('/reponse/<int:level_id>/<int:question_id>')
@@ -39,7 +51,10 @@ def reponse(level_id: int, question_id: int):
 	:param level_id: Prend l'ID du niveau (de 1 à 3)
 	:param question_id: Prend l'ID de la question (0 à 9).
 	"""
-	return render_template("reponse.html", level_id=level_id, question_id=question_id)
+	if is_map_valid(level_id) and is_question_valid(question_id):
+		return render_template("reponse.html", level_id=level_id, question_id=question_id)
+	else:
+		abort(403)
 
 
 if __name__ == '__main__':
