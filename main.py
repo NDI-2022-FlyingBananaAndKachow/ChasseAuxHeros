c3364ee8cfd6ddd26c9aae70ba15699c56f40aef
sd18ub <60876384+sd18ub@users.noreply.github.com>
2022-12-01 19:34:21 +0100

    edit msg

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__, template_folder="templates/", static_folder='static/')
 
 @app.route('/')
 def index():
-	return "Hello world!"
+	return "Hello world! and easy win !!!!!!!!!!"
 
 
 if __name__ == '__main__':
