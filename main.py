e28d0db07d0a50d57903beaa91dc96fdb5227359
megat69 <assassincraft218@gmail.com>
2022-12-01 19:39:18 +0100

    Improved for PEP8

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from markupsafe import escape
 
 app = Flask(__name__, template_folder="templates/", static_folder='static/')
 
+
 @app.route('/')
 def index():
 	return "Hello world! and easy win !!!!!!!!!!"
